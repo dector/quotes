b7b6f17e4b914c8e8f8a4524328aa2933c223e6b
Denys M <dector9@gmail.com>
2016-01-30 17:49:43 +0200

    Bring some colors

--- app/src/main/java/io/github/dector/quotes/MainActivity.kt
+++ app/src/main/java/io/github/dector/quotes/MainActivity.kt
@@ -28,6 +28,13 @@ class MainActivity : AppCompatActivity() {
 
         presenter.init()
     }
+
+    override fun onResume() {
+        super.onResume()
+        window.decorView.systemUiVisibility = android.view.View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                android.view.View.SYSTEM_UI_FLAG_FULLSCREEN or
+                android.view.View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
 }
 
 interface IActionListener {
@@ -39,6 +46,8 @@ class Presenter(val quotesFactory: QuotesFactory) : IActionListener {
 
     lateinit var view: View
 
+    val pallete = Palette()
+
     fun init() {
         view.setup()
 
@@ -49,6 +58,10 @@ class Presenter(val quotesFactory: QuotesFactory) : IActionListener {
         val quote = quotesFactory.randomQuote()
         view.showAuthor(quote.author)
         view.showQuote(quote.quote)
+
+        val colors = pallete.random()
+        view.textColor(colors.text)
+        view.backgroundColor(colors.background)
     }
 }
 
@@ -56,13 +69,16 @@ class View(val layout: android.view.View) {
 
     lateinit var actionListener: IActionListener
 
+    private lateinit var rootView: android.view.View
     private lateinit var quoteView: TextView
     private lateinit var authorView: TextView
 
     fun setup() {
+        rootView = layout.findViewById(R.id.main_root)
         quoteView = layout.findViewById(R.id.main_quote) as TextView
         authorView = layout.findViewById(R.id.main_author) as TextView
-        layout.findViewById(R.id.main_root).setOnClickListener { actionListener.displayQuote() }
+
+        rootView.setOnClickListener { actionListener.displayQuote() }
     }
 
     fun showQuote(quote: String) {
@@ -71,5 +87,14 @@ class View(val layout: android.view.View) {
 
     fun showAuthor(author: String) {
         authorView.text = author
+    }
+
+    fun textColor(color: Color) {
+        quoteView.setTextColor(color.solidValue())
+        authorView.setTextColor(color.solidValue())
+    }
+
+    fun backgroundColor(color: Color) {
+        rootView.setBackgroundColor(color.solidValue())
     }
 }
---
+++ app/src/main/java/io/github/dector/quotes/Palette.kt
@@ -0,0 +1,61 @@
+package io.github.dector.quotes
+
+import io.github.dector.quotes.Color.*
+import java.util.*
+
+class Palette {
+
+    private val colors = arrayOf(
+            ColorPair(WHITE, RED),
+            ColorPair(WHITE, PINK),
+            ColorPair(WHITE, PURPLE),
+            ColorPair(WHITE, DEEP_PURPLE),
+            ColorPair(WHITE, INDIGO),
+            ColorPair(WHITE, BLUE),
+            ColorPair(BLACK, LIGHT_BLUE),
+            ColorPair(BLACK, CYAN),
+            ColorPair(WHITE, TEAL),
+            ColorPair(BLACK, GREEN),
+            ColorPair(BLACK, LIGHT_GREEN),
+            ColorPair(BLACK, LIME),
+            ColorPair(BLACK, YELLOW),
+            ColorPair(BLACK, AMBER),
+            ColorPair(BLACK, ORANGE),
+            ColorPair(WHITE, DEEP_ORANGE),
+            ColorPair(WHITE, BROWN),
+            ColorPair(BLACK, GREY),
+            ColorPair(WHITE, BLUE_GREY)
+    )
+
+    fun random() = colors[Random().nextInt(colors.size)]
+}
+
+data class ColorPair(val text: Color, val background: Color)
+
+enum class Color(val value: Int) {
+    WHITE(0xffffff),
+    BLACK(0x000000),
+
+    // 500 colors
+    RED(0xf44336),
+    PINK(0xe91e63),
+    PURPLE(0x9c27b0),
+    DEEP_PURPLE(0x673ab7),
+    INDIGO(0x3f51b5),
+    BLUE(0x2196f3),
+    LIGHT_BLUE(0x03a9f4),
+    CYAN(0x00bcd4),
+    TEAL(0x009688),
+    GREEN(0x4caf50),
+    LIGHT_GREEN(0x8bc34a),
+    LIME(0xcddc39),
+    YELLOW(0xffeb3b),
+    AMBER(0xffc107),
+    ORANGE(0xff9800),
+    DEEP_ORANGE(0xff5722),
+    BROWN(0x795548),
+    GREY(0x9e9e9e),
+    BLUE_GREY(0x607d8b);
+
+    fun solidValue() = 0xff000000.toInt() or value
+}
