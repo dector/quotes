5269952762987ad59ae66d18006bc2be6d560137
Denys M <dector9@gmail.com>
2016-02-21 01:58:34 +0200

    Json response with Klaxon

--- api-server/src/main/java/io/github/dector/quote/api_server/main.kt
+++ api-server/src/main/java/io/github/dector/quote/api_server/main.kt
@@ -1,12 +1,30 @@
 package io.github.dector.quote.api_server
 
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import io.github.dector.quotes.domain.Quote
 import org.wasabi.app.AppServer
 
 fun main(args: Array<String>) {
     val server = AppServer()
 
-    server.get("/quotes", { response.send("Hello, fucking world!") })
+    server.get("/quotes", {
+        response.send(getQuotesJsonString())
+    })
 
     server.configuration.port = 1304
     server.start()
 }
+
+fun getQuotesJsonString(): String {
+    val quotes = listOf(Quote("Zhinka, jak elektronika - lamajet'sja", "SoloWayKo"))
+
+    return quotes.toJsonArray().toJsonString(true)
+}
+
+fun List<Quote>.toJsonArray(): JsonArray<JsonObject> {
+    val list = map { it.toJson() }
+    return JsonArray(list)
+}
+
+fun Quote.toJson() = JsonObject(mapOf("quote" to quote, "author" to author))
