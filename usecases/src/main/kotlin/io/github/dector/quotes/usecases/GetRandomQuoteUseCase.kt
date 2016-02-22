4bf106e6e7e57eec2717bb4a4e4f5de6bb341c30
Denys M <dector9@gmail.com>
2016-02-22 19:46:40 +0200

    Provide two callbacks for use case

--- usecases/src/main/kotlin/io/github/dector/quotes/usecases/GetRandomQuoteUseCase.kt
+++ usecases/src/main/kotlin/io/github/dector/quotes/usecases/GetRandomQuoteUseCase.kt
@@ -7,20 +7,13 @@ import io.github.dector.quotes.repositories.QuotesCriteria
 
 class GetRandomQuoteUseCase(val repository: IQuotesRepository,
                             val randomizer: List<Quote>.() -> Quote? = { this.random() },
-                            jobExecutor: (() -> Unit) -> Unit,
-                            callbackExecutor: (() -> Unit) -> Unit) : AsyncUseCase<Quote, Throwable>(jobExecutor, callbackExecutor), IGetRandomQuoteUseCase {
+                            jobExecutor: (() -> Unit) -> Unit = AsyncUseCase.plainExecutor,
+                            callbackExecutor: (() -> Unit) -> Unit = AsyncUseCase.plainExecutor) :
+        AsyncUseCase<Quote, Throwable>(jobExecutor, callbackExecutor), IGetRandomQuoteUseCase {
 
-    override fun fetchData(): Quote? {
-        return repository
-                .get(QuotesCriteria.Anything())
-                .randomizer()
-    }
+    override fun fetchData() = repository
+            .get(QuotesCriteria.Anything())
+            .randomizer()
 
-    override fun onSucceed(data: Quote?, callback: (UseCaseResult<Quote, Throwable>) -> Unit) {
-        callback(UseCaseResult(data))
-    }
-
-    override fun onFailed(e: Exception, callback: (UseCaseResult<Quote, Throwable>) -> Unit) {
-        callback(UseCaseResult(null, e))
-    }
+    override fun fetchError(e: Exception) = e
 }
