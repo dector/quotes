bc6ce3d3e8a1949330bfb7fd8dc4656108ea29e1
Denys M <dector9@gmail.com>
2019-10-14 01:52:04 +0400

    More tests for in-memory quotes storage.

--- storage/src/jvmTest/kotlin/io/github/dector/quotes/storage/InMemoryQuotesStorageTest.kt
+++ storage/src/jvmTest/kotlin/io/github/dector/quotes/storage/InMemoryQuotesStorageTest.kt
@@ -10,7 +10,7 @@ import io.kotlintest.specs.BehaviorSpec
 class InMemoryQuotesStorageTest : BehaviorSpec({
 
     Given("In-memory storage") {
-        val storage = InMemoryQuotesStorage()
+        val storage: QuotesStorage = InMemoryQuotesStorage()
 
         Then("Is empty") {
             storage.count() shouldBe 0
@@ -35,6 +35,78 @@ class InMemoryQuotesStorageTest : BehaviorSpec({
 
                 shouldThrow<IllegalStateException> {
                     storage.insert(quote2)
+                }
+            }
+
+            And("Upsert item same uuid") {
+                val quote3 = Quote(uuid, "Quote 3", "Author 3")
+
+                storage.insert(quote3)
+
+                Then("Stored quote should be updated") {
+                    storage.count() shouldBe 1
+
+                    val storedQuote = storage.find(uuid)!!
+                    storedQuote.content shouldBe quote3.content
+                    storedQuote.author shouldBe quote3.author
+                }
+            }
+        }
+
+        And("Has few items") {
+            val quote1 = Quote("Quote 1", "Author 1")
+            val quote2 = Quote("Quote 2", "Author 2")
+            val quote3 = Quote("Quote 3", "Author 3")
+
+            storage.insert(quote1)
+            storage.insert(quote2)
+            storage.insert(quote3)
+
+            When("Request item by uuid") {
+                val item = storage.find(quote2.uuid)
+
+                Then("Operation is not desctructive") {
+                    storage.count() shouldBe 3
+                }
+
+                Then("It should return correct item") {
+                    item shouldBe quote2
+                }
+            }
+        }
+
+        And("Has few other items") {
+            val uuid = Uuid.new()
+            storage.insert(Quote(uuid, "Quote 1", "Author 1"))
+            storage.insert(Quote("Quote 2", "Author 2"))
+            storage.insert(Quote("Quote 3", "Author 3"))
+
+            Then("") {
+                storage.count() shouldBe 3
+            }
+
+            When("Add few unique items") {
+                storage.insert(Quote("Quote 4", "Author 4"))
+                storage.insert(Quote("Quote 5", "Author 5"))
+
+                Then("") {
+                    storage.count() shouldBe 5
+                }
+
+                And("Update existing item") {
+                    storage.upsert(Quote(uuid, "Quote 6", "Author 6"))
+
+                    Then("") {
+                        storage.count() shouldBe 5
+                    }
+
+                    And("Delete existing item") {
+                        storage.delete(uuid)
+
+                        Then("") {
+                            storage.count() shouldBe 4
+                        }
+                    }
                 }
             }
         }
