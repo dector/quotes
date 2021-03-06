3ed4f9db8b6a1569ffd23c9f256ad3b3eb244631
Denys M <dector@dector.space>
2021-03-06 11:50:41 +0400

    Add empty composable on main screen

--- quotes2-android/src/main/kotlin/space/dector/quotes/android/MainActivity.kt
+++ quotes2-android/src/main/kotlin/space/dector/quotes/android/MainActivity.kt
@@ -3,9 +3,14 @@ package space.dector.quotes.android
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 
@@ -15,20 +20,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MainScreen()
+            AppTheme {
+                MainScreen()
+            }
         }
     }
 }
 
 @Composable
 fun MainScreen() {
-    MaterialTheme {
-        Text("Quotes")
+    Surface(
+        color = Color.Black,
+    ) {
+        Text("TBD")
     }
 }
 
 @Composable
-@Preview(name = "MainScreen", showBackground = true)
-fun MainScreen_Preview() {
-    MainScreen()
+@Preview(name = "MainScreen Dark", showBackground = true)
+fun MainScreen_Dark_Preview() {
+    AppTheme(
+        isDark = true,
+    ) {
+        MainScreen()
+    }
+}
+
+@Composable
+fun AppTheme(
+    isDark: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (isDark)
+        darkColors()
+    else lightColors()
+
+    MaterialTheme(
+        colors = colors,
+        content = content,
+    )
 }
