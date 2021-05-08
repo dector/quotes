74ea6b9c0d6423b3fc5c69452d4e95d2513886b9
Denys M <dector@dector.space>
2021-05-09 00:40:18 +0400

    Update AppCompat to 1.3.0-rc01

--- quotes2-android/build.gradle.kts
+++ quotes2-android/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.3.0-rc01")
 
     implementation("androidx.compose.ui:ui:1.0.0-beta06")
     implementation("androidx.compose.ui:ui-tooling:1.0.0-beta06")
